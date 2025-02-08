800144e8072fb73be44b8083c9ad7b683a357cf4
Marcus Ng <105082404+Marcus990@users.noreply.github.com>
2025-02-08 01:54:06 -0500

    Update sap_model.py

--- SAPChallenge/sap_model.py
+++ SAPChallenge/sap_model.py
@@ -1,2 +1,2 @@
-# This file contains the code that you used to train your model on the SAP dataset.
-# Feel free to add any additional files of code that you used to train your model.
+# This file contains the code that you used to train your model on the SAP dataset(s).
+# Feel free to add any additional files of code that you used to train your model in this folder.
