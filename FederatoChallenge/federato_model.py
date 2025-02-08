6ee823bae73b6227ae8910a256d8cbc5bacbe87c
Marcus Ng <105082404+Marcus990@users.noreply.github.com>
2025-02-08 01:54:50 -0500

    Update federato_model.py

--- FederatoChallenge/federato_model.py
+++ FederatoChallenge/federato_model.py
@@ -1,1 +1,2 @@
-# This file should contain code that you used to train your model on the Federato provided dataset.
+# This file contains the code that you used to train your model on the Federato dataset(s).
+# Feel free to add any additional files of code that you used to train your model in this folder.
