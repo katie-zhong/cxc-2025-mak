fb68b5bcdb9de440900ce5d4f91e22ae901a5d69
Marcus Ng <105082404+Marcus990@users.noreply.github.com>
2025-02-08 01:53:15 -0500

    Create runql_model.py

---
+++ RunQLChallenge/runql_model.py
@@ -0,0 +1,2 @@
+# This file contains the code that you used to train your model on the RunQL dataset(s).
+# Feel free to add any additional files of code that you used to train your model in this folder.
