efaf71b3354d91ac4a41a7decd07f3395126401e
Katie Zhong <tokatiezhong@gmail.com>
2025-02-09 12:38:34 -0500

    Delete TouchBistroChallenge directory

--- TouchBistroChallenge/touchbistro_model.py
+++
@@ -1,2 +0,0 @@
-# This file contains the code that you used to train your model on the TouchBistro dataset(s).
-# Feel free to add any additional files of code that you used to train your model in this folder.
